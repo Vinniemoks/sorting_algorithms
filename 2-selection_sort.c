9953969de3e93f1374f6122f266cb9366e17e894
Vincent Mokua <mokua.vinny@gmail.com>
2024-10-31 12:21:32 -0700

    tasks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,35 @@
 #include "sort.h"
+#include <string.h>
 
 /**
- * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm.
- * @array: The array of integers to sort.
- * @size: The size of the array.
+ * selection_sort - function that sorts an array of integers
+ * in ascending order
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-
-	if (array == NULL || size < 2)
-		return;
+	size_t i, j, min;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[min])
+			{
+				min = j;
+				continue;
+			}
 		}
-
-		if (min_index != i)
+		if (min != i)
 		{
-			_swapints(array, i, min_index);
-
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
 			print_array(array, size);
 		}
 	}
