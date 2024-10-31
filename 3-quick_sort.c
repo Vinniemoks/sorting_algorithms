59190ffc57c6169d594ad698f4856e592042d057
Vincent Mokua <mokua.vinny@gmail.com>
2024-10-31 12:34:31 -0700

    init

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,75 +1,79 @@
+//Introduction
+//____________________________
+/* Quicksort is a divide and conquer algorithm.
+Here we choose an element of array as pivot and partition the 
+given array around this pivot.
+We can choose the pivot in different ways.
+ 1. First element as pivot
+ 2. Last element as pivot
+ 3. Random element as pivot 
+ 4. median as pivot
+ Main aim is to find the place of pivot where it should be present in the sorted array.
+ The default implementation of Quicksort is not stable but it can be made.
+ The Quicksort is not adaptive.
+*/
+//_______________________________
+// Time complexity
+/* 
+   1. Worst case:
+       O(n^2) the case of already sorted list.
+   2. Best case:
+       O(nlogn) median element as pivot
+   3. Avearge case:
+       O(nlogn)
+*/
+//_______________________________
+// Code
+#include <stdio.h>
+#include<stdlib.h>
 #include "sort.h"
 
-/**
- * quick_sort - Sorts an array of integers in ascending order using
- *              the Quick sort algorithm (Lomuto partition scheme).
- * @array: The array of integers to sort.
- * @size: The size of the array.
- */
-void quick_sort(int *array, size_t size)
+void quick_sort(int*array, size_t size);
 {
-	if (array == NULL || size < 2)
-		return;
-
-	quicksort(array, 0, size - 1, size);
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+// This function returns the index of pivot where it should be present in sortd array.
+// We are choosing first element as our pivot.
+int partition(int A[],int l,int h)
+{
+    int pivot=A[l];
+    int i=l,j=h;
+    do
+    {
+        do{i++;}while(A[i]<=pivot);
+        do{j--;}while(A[j]>pivot);
+        if(i<j)
+            swap(&A[i],&A[j]);
+    }while(i<j);
+    swap(&A[l],&A[j]);
+    return j;
+}
+// This is recursive function for quicksort.
+void QuickSort(int A[],int l,int h)
+{
+    int j;
+    if(l<h)
+    {
+        j=partition(A,l,h);
+        QuickSort(A,l,j);
+        QuickSort(A,j+1,h);
+    }
 }
 
-/**
- * quicksort - Recursively sorts the array using Quick Sort.
- * @array: The array to sort.
- * @low: The starting index of the subarray.
- * @high: The ending index of the subarray.
- * @size: The total size of the array.
- */
-void quicksort(int *array, int low, int high, size_t size)
+int main()
 {
-	int pivot_index;
+    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
 
-	if (low < high)
-	{
-		pivot_index = lomuto_partition(array, low, high, size);
+    QuickSort(A, 0, n);
 
-		quicksort(array, low, pivot_index - 1, size);
-		quicksort(array, pivot_index + 1, high, size);
-	}
+    for(i=0;i<10;i++)
+        printf("%d ",A[i]);
+    printf("\n");
+
+    return 0;
 }
 
-/**
- * lomuto_partition - Partitions the array using Lomuto partition scheme.
- * @array: The array to partition.
- * @low: The starting index of the subarray.
- * @high: The ending index of the subarray (pivot).
- * @size: The total size of the array.
- *
- * Return: The index of the pivot element after partition.
- */
-int lomuto_partition(int *array, int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int i = low - 1;
-	int j, temp;
-
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (i != j)
-			{
-				_swapints(array, i, j);
-				print_array(array, size);
-			}
-		}
-	}
-
-	if (array[high] != array[i + 1])
-	{
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-
-		print_array(array, size);
-	}
-
-	return (i + 1);
-}
+// Output:
+// 3 5 7 9 10 11 12 13 16 24
