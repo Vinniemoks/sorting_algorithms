4b8ef2cfd3a031091e8984eca27ed9e152ced56b
Vincent Mokua <mokua.vinny@gmail.com>
2024-11-02 13:00:25 -0700

    init

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,79 +1,78 @@
-//Introduction
-//____________________________
-/* Quicksort is a divide and conquer algorithm.
-Here we choose an element of array as pivot and partition the 
-given array around this pivot.
-We can choose the pivot in different ways.
- 1. First element as pivot
- 2. Last element as pivot
- 3. Random element as pivot 
- 4. median as pivot
- Main aim is to find the place of pivot where it should be present in the sorted array.
- The default implementation of Quicksort is not stable but it can be made.
- The Quicksort is not adaptive.
-*/
-//_______________________________
-// Time complexity
-/* 
-   1. Worst case:
-       O(n^2) the case of already sorted list.
-   2. Best case:
-       O(nlogn) median element as pivot
-   3. Avearge case:
-       O(nlogn)
-*/
-//_______________________________
-// Code
-#include <stdio.h>
-#include<stdlib.h>
 #include "sort.h"
-
-void quick_sort(int*array, size_t size);
+/**
+*swap - the positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, ssize_t item1, ssize_t item2)
 {
-    int temp=*x;
-    *x=*y;
-    *y=temp;
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
 }
-// This function returns the index of pivot where it should be present in sortd array.
-// We are choosing first element as our pivot.
-int partition(int A[],int l,int h)
+/**
+ *lomuto_partition - lomuto partition sorting scheme implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: size array
+ *Return: return the position of the last element sorted
+ */
+int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
 {
-    int pivot=A[l];
-    int i=l,j=h;
-    do
-    {
-        do{i++;}while(A[i]<=pivot);
-        do{j--;}while(A[j]>pivot);
-        if(i<j)
-            swap(&A[i],&A[j]);
-    }while(i<j);
-    swap(&A[l],&A[j]);
-    return j;
+	int pivot = array[last];
+	ssize_t current = first, finder;
+
+	for (finder = first; finder < last; finder++)
+	{
+		if (array[finder] < pivot)
+		{
+			if (array[current] != array[finder])
+			{
+				swap(array, current, finder);
+				print_array(array, size);
+			}
+			current++;
+		}
+	}
+	if (array[current] != array[last])
+	{
+		swap(array, current, last);
+		print_array(array, size);
+	}
+	return (current);
 }
-// This is recursive function for quicksort.
-void QuickSort(int A[],int l,int h)
+/**
+ *qs - qucksort algorithm implementation
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: array size
+ */
+void qs(int *array, ssize_t first, ssize_t last, int size)
 {
-    int j;
-    if(l<h)
-    {
-        j=partition(A,l,h);
-        QuickSort(A,l,j);
-        QuickSort(A,j+1,h);
-    }
-}
+	ssize_t position = 0;
 
-int main()
+
+	if (first < last)
+	{
+		position = lomuto_partition(array, first, last, size);
+
+		qs(array, first, position - 1, size);
+		qs(array, position + 1, last, size);
+	}
+}
+/**
+ *quick_sort - prepare the terrain to quicksort algorithm
+ *@array: array
+ *@size: array size
+ */
+void quick_sort(int *array, size_t size)
 {
-    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
-
-    QuickSort(A, 0, n);
-
-    for(i=0;i<10;i++)
-        printf("%d ",A[i]);
-    printf("\n");
-
-    return 0;
+	if (!array || size < 2)
+		return;
+	qs(array, 0, size - 1, size);
 }
-
-// Output:
-// 3 5 7 9 10 11 12 13 16 24
